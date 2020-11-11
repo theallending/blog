9248ffcf803bd504369e442869c8f48cd44c537a
theallending <73499753+theallending@users.noreply.github.com>
2020-11-10 21:43:21 -0500

    Update blogTemplate.js

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -2,6 +2,7 @@ import React from "react"
 import Helmet from 'react-helmet';
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+require(`katex/dist/katex.min.css`)
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
